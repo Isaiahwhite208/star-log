5e85c66d1818eb51add6736aa9014617f60d09f3
Isaiah White <isaiahwhite208@gmail.com>
2018-08-30 10:37:07 -0600

    push

--- server-assets/routes/logs.js
+++ server-assets/routes/logs.js
@@ -3,20 +3,31 @@ let Logs = require('../models/log')
 
 //GET ALL
 router.get('/api/logs', (req, res, next) => {
- Logs.find({})
-  .then(logs => {
-   res.status(200).send(logs)
-  })
-  .catch(err => {
-   res.status(400).send(err)
-  })
+  Logs.find({})
+    .then(logs => {
+      res.status(200).send(logs)
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
 })
 
 // Get User Logs
 router.get('api/userlogs', (req, res) => {
- Logs.find({ author: req.session.uid })
-  .then(logs => {
-   res.status(200).send(logs)
-  })
+  Logs.find({ author: req.session.uid })
+    .then(logs => {
+      res.status(200).send(logs)
+    })
 })
 
+router.delete('api/logs/:logId', (req, res) => {
+  Logs.findById(req.params.logId)
+    .then(log => {
+      if (log.author != req.session.uid) {
+        res.status(401).send('Cannot Delete Logs that are not yours')
+      }
+      log.remove(() => {
+        res.status(200).send('DELORTED')
+      })
+    })
+})
